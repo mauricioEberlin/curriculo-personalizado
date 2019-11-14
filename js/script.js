f93ae3605796757be927c6d7cd736c4659f540db
Maurício Eberlin Pereira <mauricio.eberlin@hotmail.com>
2019-11-14 14:18:00 -0300

    [09] - Adaptação para Mobile 2: Hamburguer adicionado

---
+++ js/script.js
@@ -0,0 +1,19 @@
+window.document.onload = function(e){ 
+    var $body = document.querySelector('body');
+    $body.classList.remove('no-js')
+    $body.classList.add('js')
+}
+
+var botaoHamburguer = document.getElementById("btnHamburguer");
+var menuEscondido = document.getElementById("menu");
+
+botaoHamburguer.onclick = function(e){
+    if(menuEscondido.classList.contains("hidden")){
+        menuEscondido.classList.remove("hidden");
+        botaoHamburguer.classList.add("active");
+    }else{
+        menuEscondido.classList.add("hidden");
+        botaoHamburguer.classList.remove("active");
+    }
+}
+
